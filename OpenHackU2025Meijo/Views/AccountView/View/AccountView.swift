3dfe200eef2c033da6dfe2f195eb51a43483b893
s0-sh1r0 <231205030@ccmailg.meijo-u.ac.jp>
2025-11-11 18:09:52 +0900

    AccountView実装

--- OpenHackU2025Meijo/Views/AccountView/View/AccountView.swift
+++ OpenHackU2025Meijo/Views/AccountView/View/AccountView.swift
@@ -2,8 +2,51 @@ import SwiftUI
 
 struct AccountView: View {
     var body: some View {
-        VStack {
-            Text("AccountView")
+        VStack(alignment: .leading) {
+            HStack {
+                HStack(spacing: 24) {
+                    Circle()
+                        .frame(width: 100, height: 100)
+                        .overlay {
+                            Image(systemName: "person.fill")
+                                .foregroundStyle(.pink)
+                                .font(.system(size: 50))
+                        }
+                    
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text("username")
+                            .foregroundStyle(.white)
+                        
+                        HStack(spacing: 24) {
+                            VStack {
+                                Text("100")
+                                    .foregroundStyle(.white)
+                                Text("問題集")
+                                    .foregroundStyle(.white)
+                            }
+                            
+                            VStack {
+                                Text("100")
+                                    .foregroundStyle(.white)
+                                Text("継続日数")
+                                    .foregroundStyle(.white)
+                            }
+                            
+                            VStack {
+                                Text("100")
+                                    .foregroundStyle(.white)
+                                Text("友達")
+                                    .foregroundStyle(.white)
+                            }
+                        }
+                    }
+                }
+                .padding()
+                
+                Spacer()
+            }
+            
+            Spacer()
         }
         .fullBackground()
     }
