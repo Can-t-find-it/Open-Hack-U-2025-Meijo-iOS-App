1f127a328e13d51911e0515f5a9c80bba4c2e39d
s0-sh1r0 <231205030@ccmailg.meijo-u.ac.jp>
2025-11-12 01:46:08 +0900

    AddTextbookView実装

--- OpenHackU2025Meijo/Views/AddTextbookView/View/AddTextbookView.swift
+++ OpenHackU2025Meijo/Views/AddTextbookView/View/AddTextbookView.swift
@@ -1,9 +1,31 @@
 import SwiftUI
+import AppColorTheme
 
 struct AddTextbookView: View {
+    @State private var prompt: String = ""
     var body: some View {
         VStack {
-            Text("AddTextbooksView")
+            HStack {
+                Image(systemName: "plus.circle.fill")
+                    .foregroundColor(.white)
+                    .font(.system(size: 40))
+                
+                ZStack(alignment: .trailing) {
+                    TextField("例) 基本情報技術者試験", text: $prompt)
+                        .padding(.vertical, 10)
+                        .padding(.horizontal, 16)
+                        .padding(.trailing, 40)
+                        .foregroundColor(.black)
+                        .background(Color.white)
+                        .cornerRadius(10)
+                    
+                    Image(systemName: "arrow.up.circle.fill")
+                        .foregroundColor(AppColorToken.background.surface)
+                        .font(.system(size: 28))
+                        .padding(.trailing, 10)
+                }
+            }
+            .padding()
         }
         .fullBackground()
     }
